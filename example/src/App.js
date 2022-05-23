6b9aaf59f6af2d57cf163ef641f7b7e7758c2943
baldulin <baldulin@systemli.org>
2022-05-23 17:57:07 +0200

    fix: Removes warnings

--- example/src/App.js
+++ example/src/App.js
@@ -1,30 +1,24 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 
 import { Map, Marker, ZoomControl } from 'pigeon-maps'
 import { SuperCluster } from 'pigeon-cluster'
 import points from './points.json'
 
 const App = () => {
-  const [state, setState] = useState({
-    center: [12.17506, 53.7942],
-    zoom: 6
-  })
-  const { center, zoom } = state
-
   const markers = useMemo(
     () => points.map((coords, i) => <Marker key={i} anchor={coords} />),
-    [points]
+    []
   )
 
   return (
     <div>
       <Map
-        limitBounds='edge'
-        center={center}
-        zoom={zoom}
+        center={[12.17506, 53.7942]}
+        zoom={6}
         defaultWidth={600}
         height={400}
       >
+        <ZoomControl/>
         <SuperCluster>{markers}</SuperCluster>
       </Map>
     </div>

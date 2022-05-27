da4e59276b7b8af118e6407b180f66508d2dbb51
baldulin <baldulin@systemli.org>
2022-05-27 13:45:19 +0200

    fix: missing mapState in cloneElement

--- src/ClusterSpider.jsx
+++ src/ClusterSpider.jsx
@@ -4,19 +4,13 @@ import { Line } from './Line'
 const radialGrowth = (i) => Math.sqrt(i + 1) * 40
 const tangentialSpeed = (i) => Math.sqrt(i + 1) * Math.PI
 
-export const ClusterSpider = ({
-  component,
-  left,
-  top,
-  mapState: { width, height },
-  ...props
-}) => (
+export const ClusterSpider = ({ component, left, top, mapState, ...props }) => (
   <>
     {component.map((marker, i) => (
       <Line
         key={i}
-        width={width}
-        height={height}
+        width={mapState.width}
+        height={mapState.height}
         a={[left, top]}
         b={[
           left + radialGrowth(i) * Math.sin(tangentialSpeed(i)),
@@ -27,6 +21,7 @@ export const ClusterSpider = ({
     {component.map((marker, i) =>
       React.cloneElement(marker, {
         ...props,
+        mapState,
         left: left + radialGrowth(i) * Math.sin(tangentialSpeed(i)),
         top: top + radialGrowth(i) * Math.cos(tangentialSpeed(i))
       })
